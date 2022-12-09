20305aed7734cfb2ff5c85590c806e3bf24c5d04
Udbhav <uaashu@gmail.com>
2022-12-09 14:55:59 +0530

    new route for ai

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <>
+      <div>
+        Hello World. <Link href="/ai">AI</Link>
+      </div>
+      <div>
+        Hello World. <Link href="/about">About</Link>
+      </div>
+    </>
   )
 }
