7fa072c8303c63fea430fda747e8c4360f14667a
Udbhav <uaashu@gmail.com>
2022-12-10 14:11:18 +0530

    ai

--- pages/ai/index.tsx
+++ pages/ai/index.tsx
@@ -1,10 +1,46 @@
 import Link from 'next/link'
+import { FormEvent, useState } from 'react';
 
 const AI = (props: any) => {
-  console.log(props);
+  // console.log(props);
+
+  const [prompt, setPrompt] = useState<string>('')
+  const [src, setSrc] = useState<string>('')
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    setPrompt(event.currentTarget.value);
+  }
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      fetch("https://aliveapi.cyclic.app/generateimage", {
+        "headers": {
+          "content-type": "application/json"
+        },
+        "body": JSON.stringify({
+          prompt
+        }),
+        "method": "POST",
+      }).then(async (res) => {
+        const response = await res.json();
+
+        if (response.success) {
+          setSrc(response.data)
+        }
+      })
+    } catch (error) {
+
+    }
+  }
 
   return <>
-    <div>Link params</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} />
+      </form>
+    </div>
     <div>
       <Link href="/">Home</Link>
     </div>
